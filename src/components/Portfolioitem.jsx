bba6ec15ee685d60f90cba46d1a053c5aaba47f7
dev <dev@git.com>
2025-02-22 03:23:31 +0500

    Minor Update

---
+++ src/components/Portfolioitem.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Portfolioitem({ Number, Title, Description, Link, RepositoryLink, Image, IsOdd }) {
+  return (
+      <div className="md:grid grid-cols-2 gap-15 xl:gap-5 items-center">
+          <div className={ IsOdd ? 'mb-5 md:mb-0 md:max-w-xl ms-auto order-2' : 'mb-5 md:mb-0 md:max-w-xl' }>
+              <img src={ Image } alt="Image of the portfolio item website" className='aspect-[3/2] rounded-2xl' />
+          </div>
+          <div>
+              <p className="mb-5 text-4xl md:text-5xl font-secondary font-semibold">{ Number }</p>
+              <p className="mb-7 text-3xl md:text-4xl font-secondary font-bold">{ Title }</p>
+              <p className="text-lg text-accent-300 mb-7">{ Description }</p>
+              <div className='flex gap-5'>
+                  <a href={ Link } target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>Visit <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
+                  <a href={ RepositoryLink } target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>See Code <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
+              </div>
+          </div>
+      </div>
+  )
+}
+
+export default Portfolioitem
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import portfolioImage1 from '../assets/portfolioImage1.png'
 import portfolioImage2 from '../assets/portfolioImage2.png'
 import portfolioImage3 from '../assets/portfolioImage3.png'
@@ -7,6 +8,7 @@ import portfolioImage5 from '../assets/portfolioImage5.png'
 import portfolioImage6 from '../assets/portfolioImage6.png'
 
 import Header from '../components/Header.jsx'
+import PortfolioItem from '../components/Portfolioitem.jsx'
 import Footer from '../components/Footer.jsx'
 
 const Portfolio = () => {
@@ -19,95 +21,66 @@ const Portfolio = () => {
             </div>
 
             <div className='px-5 md:px-25 flex flex-col gap-30 md:gap-40 max-w-[105rem] mx-auto'>
-                <div className="md:grid grid-cols-2 gap-15 xl:gap-5 items-center">
-                    <div className='mb-5 md:mb-0 md:max-w-xl'>
-                        <img src={portfolioImage1} alt="" className='aspect-[3/2] rounded-2xl' />
-                    </div>
-                    <div>
-                        <p className="mb-5 text-4xl md:text-5xl font-secondary font-semibold">01</p>
-                        <p className="mb-7 text-3xl md:text-4xl font-secondary font-bold">Gen Z Knowledge - An educational website</p>
-                        <p className="text-lg text-accent-300 mb-7">A platform of vast knowledge. Offering courses, has a digital library, and a fruitful blog. Educators and professionals can offer courses to students worldwide, while passionate writers and experts can publish insightful articles on the blog.</p>
-                        <div className='flex gap-5'>
-                            <a href="https://genzknowledge.vercel.app/" target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>Visit <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-                            <a href="https://github.com/pakodev43/Gen-Z-Knowledge" target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>See Code <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-                        </div>
-                    </div>
-                </div>
 
-                <div className="md:grid grid-cols-2 gap-15 xl:gap-5 items-center">
-                    <div className='mb-5 md:mb-0 md:max-w-xl ms-auto order-2'>
-                        <img src={portfolioImage2} alt="" className='aspect-[3/2] rounded-2xl' />
-                    </div>
-                    <div>
-                        <p className="mb-5 text-4xl md:text-5xl font-secondary font-semibold">02</p>
-                        <p className="mb-7 text-3xl md:text-4xl font-secondary font-bold">Abode - Real Estate</p>
-                        <p className="text-lg text-accent-300 mb-7">Buy or sell homes, land, commercial properties, or any type of real estate.</p>
-                        <div className='flex gap-5'>
-                            <a href="https://aboderealestate.vercel.app/" target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>Visit <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-                            <a href="https://github.com/pakodev43/Abode" target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>See Code <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-                        </div>
-                    </div>
-                </div>
 
-                <div className="md:grid grid-cols-2 gap-15 xl:gap-5 items-center">
-                    <div className='mb-5 md:mb-0 md:max-w-xl'>
-                        <img src={portfolioImage3} alt="" className='aspect-[3/2] rounded-2xl' />
-                    </div>
-                    <div>
-                        <p className="mb-5 text-4xl md:text-5xl font-secondary font-semibold">03</p>
-                        <p className="mb-7 text-3xl md:text-4xl font-secondary font-bold">Jacketers</p>
-                        <p className="text-lg text-accent-300 mb-7">An E-commerce website for jackets business.</p>
-                        <div className='flex gap-5'>
-                            <a href="https://jacketers.vercel.app/" target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>Visit <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-                            <a href="https://github.com/pakodev43/Jacketers" target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>See Code <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-                        </div>
-                    </div>
-                </div>
-                
-                <div className="md:grid grid-cols-2 gap-15 xl:gap-5 items-center">
-                    <div className='mb-5 md:mb-0 md:max-w-xl ms-auto order-2'>
-                        <img src={portfolioImage4} alt="" className='aspect-[3/2] rounded-2xl' />
-                    </div>
-                    <div>
-                        <p className="mb-5 text-4xl md:text-5xl font-secondary font-semibold">04</p>
-                        <p className="mb-7 text-3xl md:text-4xl font-secondary font-bold">Abdullah Khan's Portfolio Website</p>
-                        <p className="text-lg text-accent-300 mb-7">A portfolio / personal website template. Having about, services, portfolio, testimonials, and contact sections.</p>
-                        <div className='flex gap-5'>
-                            <a href="https://pakodev43.github.io/Portfolio-Website-Template-2/" target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>Visit <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-                            <a href="https://github.com/pakodev43/Portfolio-Website-Template-2" target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>See Code <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-                        </div>
-                    </div>
-                </div>
+                <PortfolioItem
+                    Number="01"
+                    Title="Gen Z Knowledge - An educational website"
+                    Description="A platform of vast knowledge. Offering courses, has a digital library, and a fruitful blog. Educators and professionals can offer courses to students worldwide, while passionate writers and experts can publish insightful articles on the blog."
+                    Link="https://genzknowledge.vercel.app/"
+                    RepositoryLink="https://github.com/pakodev43/Gen-Z-Knowledge"
+                    Image={ portfolioImage1 }
+                    IsOdd
+                />
 
-                <div className="md:grid grid-cols-2 gap-15 xl:gap-5 items-center">
-                    <div className='mb-5 md:mb-0 md:max-w-xl'>
-                        <img src={portfolioImage5} alt="" className='aspect-[3/2] rounded-2xl' />
-                    </div>
-                    <div>
-                        <p className="mb-5 text-4xl md:text-5xl font-secondary font-semibold">05</p>
-                        <p className="mb-7 text-3xl md:text-4xl font-secondary font-bold">Adam's Professional Personal Website</p>
-                        <p className="text-lg text-accent-300 mb-7">A professional and elegent personal / portfolio website template having multiple sections.</p>
-                        <div className='flex gap-5'>
-                            <a href="https://pakodev43.github.io/Writers-Portfolio-Website-II/" target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>Visit <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-                            <a href="https://github.com/pakodev43/Writers-Portfolio-Website-II" target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>See Code <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-                        </div>
-                    </div>
-                </div>
+                <PortfolioItem
+                    Number="02"
+                    Title="Abode - Real Estate"
+                    Description="Buy or sell homes, land, commercial properties, or any type of real estate."
+                    Link="https://aboderealestate.vercel.app/"
+                    RepositoryLink="https://github.com/pakodev43/Abode"
+                    Image={ portfolioImage2 }
+                />
 
-                <div className="md:grid grid-cols-2 gap-15 xl:gap-5 items-center">
-                    <div className='mb-5 md:mb-0 md:max-w-xl ms-auto order-2'>
-                        <img src={portfolioImage6} alt="" className='aspect-[3/2] rounded-2xl' />
-                    </div>
-                    <div>
-                        <p className="mb-5 text-4xl md:text-5xl font-secondary font-semibold">06</p>
-                        <p className="mb-7 text-3xl md:text-4xl font-secondary font-bold">Hiking and Traveling Blog Landing Page</p>
-                        <p className="text-lg text-accent-300 mb-7">The landing page for a hiking and traveling blog. It has simple animations on the hero section images.</p>
-                        <div className='flex gap-5'>
-                            <a href="https://pakodev43.github.io/Hiking-and-Traveling-Blog-Landing-Page/" target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>Visit <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-                            <a href="https://github.com/pakodev43/Hiking-and-Traveling-Blog-Landing-Page" target='_blank' className='flex items-center gap-3 bg-primary text-white border-2 hover:border-primary py-3 px-5.5 rounded-xl font-medium'>See Code <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8335 9.16671L17.6668 2.33337" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M18.3335 5.66663V1.66663H14.3335" stroke="#F4F4F5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9.1665 1.66663H7.49984C3.33317 1.66663 1.6665 3.33329 1.6665 7.49996V12.5C1.6665 16.6666 3.33317 18.3333 7.49984 18.3333H12.4998C16.6665 18.3333 18.3332 16.6666 18.3332 12.5V10.8333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></a>
-                        </div>
-                    </div>
-                </div>
+                <PortfolioItem
+                    Number="03"
+                    Title="Jacketers"
+                    Description="An E-commerce website for jackets business."
+                    Link="https://jacketers.vercel.app/"
+                    RepositoryLink="https://github.com/pakodev43/Jacketers"
+                    Image={portfolioImage3}
+                    IsOdd
+                />
+
+                <PortfolioItem
+                    Number="04"
+                    Title="Abdullah Khan's Portfolio Website"
+                    Description="A portfolio / personal website template. Having about, services, portfolio, testimonials, and contact sections."
+                    Link="https://pakodev43.github.io/Portfolio-Website-Template-2/"
+                    RepositoryLink="https://github.com/pakodev43/Portfolio-Website-Template-2"
+                    Image={ portfolioImage4 }
+                />
+
+                <PortfolioItem
+                    Number="05"
+                    Title="Adam's Professional Personal Website"
+                    Description="A professional and elegent personal / portfolio website template having multiple sections."
+                    Link="https://pakodev43.github.io/Writers-Portfolio-Website-II/"
+                    RepositoryLink="https://github.com/pakodev43/Writers-Portfolio-Website-II"
+                    Image={ portfolioImage5 }
+                    IsOdd
+                />
+
+                <PortfolioItem
+                    Number="06"
+                    Title="Hiking and Traveling Blog Landing Page"
+                    Description="The landing page for a hiking and traveling blog. It has simple animations on the hero section images."
+                    Link="https://pakodev43.github.io/Hiking-and-Traveling-Blog-Landing-Page/"
+                    RepositoryLink="https://github.com/pakodev43/Hiking-and-Traveling-Blog-Landing-Page"
+                    Image={ portfolioImage6 }
+                />
+
+
             </div>
 
             <Footer />
